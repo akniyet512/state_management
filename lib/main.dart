570bc4622767d0784a875905bf0d41099e08fa10
Akniyet <akniyetbegimkulov@gmail.com>
2023-08-05 15:01:43 +0600

    Provider 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+    return ChangeNotifierProvider(
+      create: (_) => BreadCrumbProvider(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MyHomePage(),
+      ),
     );
   }
 }
@@ -25,79 +32,146 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Home Page"),
       ),
-      body: SliderInheritedNotifier(
-        sliderData: sliderData,
-        child: Builder(
-          builder: (context) {
-            return Column(
-              children: [
-                Slider(
-                  value: SliderInheritedNotifier.of(context),
-                  onChanged: (value) {
-                    sliderData.value = value;
-                  },
+      body: Column(
+        children: [
+          Consumer<BreadCrumbProvider>(
+            builder: (context, value, child) {
+              return BreadCrumbsWidget(breadCrumbs: value.items);
+            },
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const NewBreadCrumbPage(),
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Opacity(
-                        opacity: SliderInheritedNotifier.of(context),
-                        child: Container(
-                          color: Colors.yellow,
-                          height: 200,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Opacity(
-                        opacity: SliderInheritedNotifier.of(context),
-                        child: Container(
-                          color: Colors.blue,
-                          height: 200,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            );
-          },
-        ),
+              );
+            },
+            child: const Text("Add new bread brumb"),
+          ),
+          TextButton(
+            onPressed: () {
+              context.read<BreadCrumbProvider>().reset();
+            },
+            child: const Text("Reset"),
+          ),
+        ],
       ),
     );
   }
 }
 
-final SliderData sliderData = SliderData();
+class NewBreadCrumbPage extends StatefulWidget {
+  const NewBreadCrumbPage({super.key});
 
-class SliderInheritedNotifier extends InheritedNotifier<SliderData> {
-  const SliderInheritedNotifier({
-    super.key,
-    required Widget child,
-    required SliderData sliderData,
-  }) : super(
-          child: child,
-          notifier: sliderData,
-        );
+  @override
+  State<NewBreadCrumbPage> createState() => _NewBreadCrumbPageState();
+}
 
-  static double of(BuildContext context) {
-    return context
-            .dependOnInheritedWidgetOfExactType<SliderInheritedNotifier>()
-            ?.notifier
-            ?.value ??
-        0;
+class _NewBreadCrumbPageState extends State<NewBreadCrumbPage> {
+  final TextEditingController _controller = TextEditingController();
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add new bread crumb"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration:
+                const InputDecoration(hintText: "Enter your bread crumb here"),
+          ),
+          TextButton(
+            onPressed: () {
+              if (_controller.text.isNotEmpty) {
+                BreadCrumb breadCrumb = BreadCrumb(
+                  isActive: false,
+                  name: _controller.text,
+                );
+                context.read<BreadCrumbProvider>().add(breadCrumb);
+                Navigator.of(context).pop();
+              }
+            },
+            child: const Text("Add"),
+          ),
+        ],
+      ),
+    );
   }
 }
 
-class SliderData extends ChangeNotifier {
-  double _value = 0;
+class BreadCrumbsWidget extends StatelessWidget {
+  final UnmodifiableListView<BreadCrumb> breadCrumbs;
 
-  double get value => _value;
+  const BreadCrumbsWidget({
+    super.key,
+    required this.breadCrumbs,
+  });
 
-  set value(double newValue) {
-    if (newValue != _value) {
-      _value = newValue;
-      notifyListeners();
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Wrap(
+        children: breadCrumbs.map((breadCrumb) {
+          return Text(
+            breadCrumb.title,
+            style: TextStyle(
+              color: breadCrumb.isActive ? Colors.blue : Colors.black,
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class BreadCrumb {
+  bool isActive;
+  final String name;
+  final String uuid;
+
+  BreadCrumb({
+    required this.isActive,
+    required this.name,
+  }) : uuid = const Uuid().v4();
+
+  void activate() {
+    isActive = true;
+  }
+
+  @override
+  bool operator ==(covariant BreadCrumb other) => uuid == other.uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+
+  String get title => "$name ${isActive ? " > " : ""}";
+}
+
+class BreadCrumbProvider extends ChangeNotifier {
+  final List<BreadCrumb> _items = [];
+
+  UnmodifiableListView<BreadCrumb> get items => UnmodifiableListView(_items);
+
+  void add(BreadCrumb breadCrumb) {
+    for (BreadCrumb item in _items) {
+      item.activate();
     }
+    _items.add(breadCrumb);
+    notifyListeners();
+  }
+
+  void reset() {
+    _items.clear();
+    notifyListeners();
   }
 }
